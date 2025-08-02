e94f36addf7c0ef8a4ea8cdc3443420b789fd357
Parvez Al Mumin <parvezalmuminofficial@gmail.com>
2025-08-01 21:46:18 -0700

    Variables and Data Type Practice Added

---
+++ Variables and Data type/script.js
@@ -0,0 +1,16 @@
+ // ✅ NUMBER
+let age = 25;                   // Storing a number
+console.log("Age is:", age);    // Output: Age is: 25
+
+// ✅ STRING
+let name = "Parvez";            // Storing text inside double quotes
+console.log("Name is:", name);  // Output: Name is: Parvez
+
+// ✅ BOOLEAN
+let isStudent = true;                     // true or false (yes/no type logic)
+console.log("Is student:", isStudent);    // Output: Is student: true
+
+// ✅ UNDEFINED
+let futureJob;                  // Declared but not assigned a value
+console.log("Future job is:", futureJob); // Output: Future job is: undefined
+
