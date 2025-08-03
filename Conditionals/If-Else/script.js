52f86f19b483c6eb6a7e479ba902cdec41561bc6
Parvez Al Mumin <parvezalmuminofficial@gmail.com>
2025-08-03 15:25:59 -0700

    If-Else syntax, practice code added

---
+++ Conditionals/If-Else/script.js
@@ -0,0 +1,10 @@
+ let isRaining = false;
+
+if (isRaining) {
+  console.log("Take an umbrella.");
+} else {
+  console.log("Enjoy the sunshine!");
+}
+
+// Explanation:
+// Since isRaining is false, it runs the else block
