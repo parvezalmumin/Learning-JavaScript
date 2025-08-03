54864fabd4dda6072ecbfb3f6fc3a3f459967058
Parvez Al Mumin <parvezalmuminofficial@gmail.com>
2025-08-03 15:37:05 -0700

    Even or Odd Checker Code Example Added.

---
+++ Conditionals/Even-Odd Code Pactcie/script.js
@@ -0,0 +1,9 @@
+ // Change the number to test
+let number = 8;
+
+// ✅ Check if number is even or odd
+if (number % 2 === 0) {
+  console.log(number + " is even.");
+} else {
+  console.log(number + " is odd.");
+}
