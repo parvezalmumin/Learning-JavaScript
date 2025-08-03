8101515f823de9cdd3b00678ce2a56e0ef82959a
Parvez Al Mumin <parvezalmuminofficial@gmail.com>
2025-08-02 23:28:28 -0700

    Arithmetic Operators Practice Added

---
+++ Operators/Arithmetic Operators/arithmetic.js
@@ -0,0 +1,21 @@
+// ✨ Arithmetic Operators Example
+
+// Let's declare two numbers
+let num1 = 12;
+let num2 = 4;
+
+// ➕ Addition
+let sum = num1 + num2;
+console.log("Addition (num1 + num2) = ", sum);  // 12 + 4 = 16
+
+// ➖ Subtraction
+let difference = num1 - num2;
+console.log("Subtraction (num1 - num2) = ", difference);  // 12 - 4 = 8
+
+// ✖️ Multiplication
+let product = num1 * num2;
+console.log("Multiplication (num1 * num2) = ", product);  // 12 * 4 = 48
+
+// ➗ Division
+let quotient = num1 / num2;
+console.log("Division (num1 / num2) = ", quotient);  // 12 / 4 = 3
