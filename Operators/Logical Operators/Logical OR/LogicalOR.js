11bd79f6412520f07795547a49d3546518cb57f1
Parvez Al Mumin <parvezalmuminofficial@gmail.com>
2025-08-03 11:14:03 -0700

    Logical OR Practice Added

---
+++ Operators/Logical Operators/Logical OR/LogicalOR.js
@@ -0,0 +1,23 @@
+  let a = 10;
+ let b = 111;
+ console.log("a && b:", a==b || a<b); //False || True
+
+
+ let c = 9;
+ let d = 9;
+ let e = 12;
+ let f = 3;
+  console.log("Value:", c==d || e>f); //True || True
+
+  let g = 13;
+  let h = 14;
+  let i = 15;
+  let j = 16;
+  console.log("Value:", g<h || i==j); //True || False
+
+
+  let k = 17;
+  let l = 18;
+  let m = 19;
+  let n = 20;
+  console.log("Value:", k==l || m>n); //False || False
