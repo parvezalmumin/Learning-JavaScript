1852224fe3d881edb185a95ce7866016c6260258
Parvez Al Mumin <parvezalmuminofficial@gmail.com>
2025-08-03 02:21:40 -0700

    Assignment Operator Practice Added

---
+++ Operators/AssignmentOperator.js
@@ -0,0 +1,25 @@
+ // ✨ Assignment Operators Example
+
+// ➡️ Basic assignment (=)
+let number = 10;
+console.log("Initial number:", number); // 10
+
+// ➕ Add and assign (+=)
+number += 5;  // same as: number = number + 5;
+console.log("After += 5:", number); // 15
+
+// ➖ Subtract and assign (-=)
+number -= 3;  // same as: number = number - 3;
+console.log("After -= 3:", number); // 12
+
+// ✖️ Multiply and assign (*=)
+number *= 2;  // same as: number = number * 2;
+console.log("After *= 2:", number); // 24
+
+// ➗ Divide and assign (/=)
+number /= 4;  // same as: number = number / 4;
+console.log("After /= 4:", number); // 6
+
+// % Modulas
+number %=4;
+console.log("After %=4:", number); //2
