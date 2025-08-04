8cd3dae3055a9701cb6c3f6b940a262ebada69fc
Parvez Al Mumin <parvezalmuminofficial@gmail.com>
2025-08-03 17:50:05 -0700

    Ternary Operator practice code added

---
+++ Conditionals/Ternary Operator/evenodd.js
@@ -0,0 +1,3 @@
+ let num = 9;
+let message = num % 2 === 0 ? "Even" : "Odd";
+console.log(message); // "Odd"
---
+++ Conditionals/Ternary Operator/result.js
@@ -0,0 +1,5 @@
+ let age = 18;
+
+let result = age >= 18 ? "You are an adult" : "You are a minor";
+
+console.log(result);
