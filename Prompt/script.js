5b3070208b981bacbdb41a6c546dc9a6914d7729
Parvez Al Mumin <parvezalmuminofficial@gmail.com>
2025-08-03 19:49:59 -0700

    Prompt Practice Added

---
+++ Prompt/script.js
@@ -0,0 +1,5 @@
+ // Ask the user for their name using prompt
+let userName = prompt("What is your name?");
+
+// Show a greeting using alert
+alert("Hello, " + userName + "! Welcome to JavaScript 🚀");
