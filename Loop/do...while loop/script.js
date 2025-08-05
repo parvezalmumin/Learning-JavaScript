dceef9739d0b33025ca1ba74498df33da73b6a5f
Parvez Al Mumin <parvezalmuminofficial@gmail.com>
2025-08-04 23:39:37 -0700

    Do...While Practice Added

---
+++ Loop/do...while loop/script.js
@@ -0,0 +1,8 @@
+ // 🚀 Print numbers from 1 to 5 using do...while loop
+
+var i = 1; // Start from 1
+
+do {
+  console.log("Number is:", i);
+  i++; // Increment i by 1
+} while (i <= 5); // Continue as long as i is less than or equal to 5
