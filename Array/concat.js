d7b1e0ce96725e0819a48f88a0171880a3c35ffc
Parvez Al Mumin <parvezalmuminofficial@gmail.com>
2025-08-13 19:39:34 -0700

    Concat notes and code added

---
+++ Array/concat.js
@@ -0,0 +1,10 @@
+ let arr1 = [1, 2, 3];
+let arr2 = [4, 5];
+let arr3 = [6, 7];
+
+let merged = arr1.concat(arr2, arr3);
+
+console.log("arr1:", arr1);      
+console.log("arr2:", arr2);      
+console.log("arr3:", arr3);      
+console.log("merged:", merged);  // [1, 2, 3, 4, 5, 6, 7]
