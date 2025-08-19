50b513675cdf138c955b9ec3ad18c4fb0e98866f
Parvez Al Mumin <parvezalmuminofficial@gmail.com>
2025-08-19 00:57:21 -0700

    Arrow Function Code Example Added

---
+++ Function/ArrowFunction.js
@@ -0,0 +1,24 @@
+// Arrow function without parameter
+var greet = () => {
+    console.log("Hello, World!");
+};
+greet(); 
+
+// If only one parameter, parentheses are optional
+var square = x => {
+    return x * x;
+};
+console.log(square(5)); // 25
+
+// Multiple parameters need parentheses
+var add = (a, b) => {
+    return a + b;
+};
+console.log(add(3, 7)); // 10
+
+// If only one statement, we can skip return and {}
+var multiply = (a, b) => a * b;
+console.log(multiply(4, 5)); // 20
+
+
+ 
