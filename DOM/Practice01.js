e8f2540a41a7d60e1ba9b9cdce1450f5cd89e35b
Parvez Al Mumin <parvezalmuminofficial@gmail.com>
2026-02-16 10:25:47 -0800

    Practice 01 solved added

---
+++ DOM/Practice01.js
@@ -0,0 +1,6 @@
+//Create a H2 element with text - "Hello JavaScript". Append "from Apna College students"to this text using JS
+let h2 = document.querySelector("h2");
+
+console.dir(h2.innerText);
+
+h2.innerText = h2.innerText + " from Apna College Students.";
