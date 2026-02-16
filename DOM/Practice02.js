2581cc111f5913c6ac26cb6803904a8eeb3a7892
Parvez Al Mumin <parvezalmuminofficial@gmail.com>
2026-02-16 14:05:16 -0800

    Practice 02 solved added

---
+++ DOM/Practice02.js
@@ -0,0 +1,7 @@
+//Create 3 divs with common class name - "box". Access them & add some unique text to each of them
+
+let divs = document.querySelectorAll(".box");
+
+divs[0].innerText = "new unique value 1";
+divs[1].innerText = "new unique value 2";
+divs[2].innerText = "new unique value 3";
